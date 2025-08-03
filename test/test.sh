da124509c5254fcdbe6c32d2e97c5ca3bab74dbe
Michael <github@mjpeter.de>
2025-08-03 16:22:52 +0200

    ci: fix test.sh

--- test/test.sh
+++ test/test.sh
@@ -2,24 +2,24 @@
 set -e
 
 wait_for_url() {
-	echo "Testing $1"
-	max_in_s=$2
-	delay_in_s=1
-	total_in_s=0
-	while [ $total_in_s -le "$max_in_s" ]; do
-		echo "Wait ${total_in_s}s"
-		if (echo -e "GET $1\nHTTP/* 200" | hurl >/dev/null 2>&1); then
-			return 0
-		fi
-		total_in_s=$((total_in_s + delay_in_s))
-		sleep $delay_in_s
-	done
-	return 1
+  echo "Testing $1"
+  max_in_s=$2
+  delay_in_s=1
+  total_in_s=0
+  while [ $total_in_s -le "$max_in_s" ]; do
+    echo "Wait ${total_in_s}s"
+    if (echo -e "GET $1\nHTTP 200" | hurl >/dev/null 2>&1); then
+      return 0
+    fi
+    total_in_s=$((total_in_s + delay_in_s))
+    sleep $delay_in_s
+  done
+  return 1
 }
 
 cleanup() {
-	echo "Stopping container"
-	docker stop rootknecht
+  echo "Stopping container"
+  docker stop rootknecht
 }
 
 trap cleanup EXIT
