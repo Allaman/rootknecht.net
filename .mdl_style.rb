127fa685300b2a6f5607c6d9c323bceea0b59947
Michael <github@mjpeter.de>
2025-10-05 19:18:14 +0200

    chore: markdownlint-cli2 config

--- .mdl_style.rb
+++
@@ -1,7 +0,0 @@
-all
-
-# Extend line length, since each sentence should be on a separate line.
-rule 'MD013', :line_length => 99999
-
-# Lists should be surrounded by blank lines - problematic in frontmatter
-exclude_rule 'MD032'
